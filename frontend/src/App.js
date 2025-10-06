cee86d735b6efa0fdb4bed1e6e8cb0cee35dd988
Thamindu Sulakshana <89307986+ThaminduSulakshana@users.noreply.github.com>
2025-10-06 17:42:48 +0530

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,27 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { CssBaseline } from "@mui/material";
+
+import Home from "./Pages/Homepage";
+import Test from "./Pages/test";
+
+const theme = createTheme({
+  palette: { mode: "light", background: { default: "#F6F9FC" } },
+  shape: { borderRadius: 12 },
+});
+
+export default function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <BrowserRouter>
+        <Routes>
+          {/* Only Home route */}
+          <Route path="/" element={<Home />} />
+          <Route path="/test" element={<Test />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
+  );
+}
