e6e9c98efd855827daca5bee6226b65006f775d4
Thamindu Sulakshana <89307986+ThaminduSulakshana@users.noreply.github.com>
2025-10-26 10:49:46 +0530

    add items back

---
+++ backend/models/Item.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const ImageSchema = new mongoose.Schema(
+  {
+    data: { type: String, default: null }, // base64 string
+    mime: {
+      type: String,
+      enum: ["image/jpeg", "image/png", "image/webp"],
+      default: null,
+    },
+  },
+  { _id: false }
+);
+
+const ItemSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true, trim: true },
+    description: { type: String, trim: true },
+    category: { type: String, required: true, trim: true },
+    price: { type: Number, required: true, min: 0 },
+    deliveryTime: { type: String, trim: true }, // "2 Days"
+    specializations: [{ type: String, trim: true }], // ["Python", "ML", "DS"]
+    posterImage: { type: ImageSchema, default: () => ({}) },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Item", ItemSchema);
