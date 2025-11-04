0ed257b12909aae3b8e294568b354ba395000ccd
Athapaththu <99331500+sakilaathapaththu@users.noreply.github.com>
2025-11-04 17:06:12 +0530

    add changes

--- frontend/src/Utils/http.js
+++ frontend/src/Utils/http.js
@@ -1,33 +1,11 @@
-// // src/Utils/http.js
-// import axios from "axios";
 
-// // 🔧 HARD-CODE your backend origin here:
-// const API_ROOT = "http://localhost:5000"; 
-
-// const http = axios.create({
-//   baseURL: `${API_ROOT.replace(/\/+$/, "")}/api`, // -> http://localhost:5000/api
-//   headers: { "Content-Type": "application/json" },
-//   withCredentials: false,
-// });
-
-// http.interceptors.response.use(
-//   (res) => res,
-//   (err) => {
-//     const msg =
-//       err?.response?.data?.message ||
-//       err?.response?.data?.error ||
-//       err?.message ||
-//       "Request failed";
-//     return Promise.reject({ ...err, message: msg });
-//   }
-// );
-
-// export default http;
 // src/Utils/http.js
 import axios from "axios";
 
 // 🔧 HARD-CODE your backend origin here:
-const API_ROOT = "http://72.60.42.120:5501"; 
+// const API_ROOT = "http://72.60.42.120:5501"; 
+const API_ROOT = "http://api.spotmyad.blackcodedev.com"; 
+
 // const API_ROOT = "http://localhost:5501"; 
 const http = axios.create({
   baseURL: `${API_ROOT.replace(/\/+$/, "")}/api`, // -> http://localhost:5000/api
